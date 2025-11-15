6126985a5ae280091d517bc63bf9166f6c713aa4
AD Cidade <adcidademidia@gmail.com>
2025-11-15 15:27:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6666762692-bdf9d",
+  "appId": "1:50952216664:web:6e5d4f555dd9f24a8f2a2d",
+  "apiKey": "AIzaSyDpYFfStAmSLdug5n7Ae0G2uz2NJ8qNIzY",
+  "authDomain": "studio-6666762692-bdf9d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "50952216664"
+};
